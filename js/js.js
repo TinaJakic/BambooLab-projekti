bbdb6bb8f183e00af7e1d667bb4ec8eec333ab70
TinaJakic <152445313+TinaJakic@users.noreply.github.com>
2024-07-30 20:49:57 +0200

    Delete js directory

--- js/js.js
+++
@@ -1,5 +0,0 @@
-function popup() {
-    var popupWindow = window.open('https://oziz.ffos.hr/nastava20232024/tjakic_23/Z8-10/Foundation-Sites-CSS/js.html', 'prozor', 'width=600,height=400');
-    popupWindow.focus();
-}
-
